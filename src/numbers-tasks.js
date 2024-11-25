46613919b102200936103e422a02724299ce86e6
Ivan Zhuro <ivan.zhuro.by@gmail.com>
2024-11-25 16:07:13 +0300

    add: 29 passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -304,8 +304,14 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = 0;
+  let numT = num;
+  while (numT > 0) {
+    sum += numT % 10;
+    numT = Math.trunc(numT / 10);
+  }
+  return sum;
 }
 
 /**
